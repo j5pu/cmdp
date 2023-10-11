cb6a0bb687b6e4c795e88316a6628c94df00528a
root <root@example.com>
2023-10-11 21:25:02 +0200

    fix:

--- tests/test_aioclone.py
+++ tests/test_aioclone.py
@@ -6,8 +6,8 @@ Tests for aioclone.
 
 import pytest
 
-from pyrc import aioclone
-from pyrc import TempDir
+from nodeps import aioclone
+from nodeps import TempDir
 
 
 @pytest.mark.asyncio
@@ -15,5 +15,5 @@ async def test_aioclone():
     with TempDir() as tmp:
         directory = tmp / "1" / "2" / "3"
         rv = await aioclone("octocat", "Hello-World", path=directory)
-        assert rv.returncode == 0
+        assert rv == directory
         assert (directory / "README").exists()
