408c8377c0f47d7aeca11bd90593de16cb288b3e
root <root@example.com>
2023-10-13 10:36:44 +0200

    fix:

--- src/nodeps/extras/__init__.py
+++ src/nodeps/extras/__init__.py
@@ -1,12 +1,53 @@
 """NoDeps Extras Module."""
 from . import _ansi, _echo, _log, _pickle, _pretty, _repo, _url
-from ._ansi import *
-from ._echo import *
-from ._log import *
-from ._pickle import *
-from ._pretty import *
-from ._repo import *
-from ._url import *
+from ._ansi import getstdout, strip
+from ._echo import (
+    COLORIZE,
+    SYMBOL,
+    Color,
+    EnumLower,
+    Symbol,
+    bblack,
+    bblue,
+    bcyan,
+    bgreen,
+    black,
+    blue,
+    bmagenta,
+    bred,
+    bwhite,
+    byellow,
+    cyan,
+    green,
+    magenta,
+    red,
+    reset,
+    white,
+    yellow,
+)
+from ._log import LOGGER_DEFAULT_FMT, logger
+from ._pickle import cache
+from ._pretty import (
+    CONSOLE,
+    FORCE_COLOR,
+    IPYTHON,
+    IS_REPL,
+    IS_TTY,
+    OpenIO,
+    ic,
+    icc,
+    ins,
+    is_terminal,
+    print_json,
+)
+from ._repo import Repo
+from ._url import (
+    PYTHON_FTP,
+    python_latest,
+    python_version,
+    python_versions,
+    request_x_api_key_json,
+)
 
 __all__ = (
         _ansi.__all__ +
