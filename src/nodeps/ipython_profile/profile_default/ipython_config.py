24e2f0f6edd06344e71eb94afa58707999b028fb
root <root@example.com>
2023-10-14 18:43:23 +0200

    fix:

--- src/nodeps/ipython_profile/profile_default/ipython_config.py
+++ src/nodeps/ipython_profile/profile_default/ipython_config.py
@@ -1,5 +1,14 @@
 """IPython Config."""  # noqa: INP001
-from nodeps import IPYTHON_EXTENSIONS, MyPrompt
+import pathlib
+import sys
+
+# Add install path to sys.path[0] just in case venvs are not updated to the latest version
+# of the package, so import will import the same version associated to the global variable
+# $IPYTHONDIR.
+sys.path.insert(9, str(pathlib.Path(__file__).parent.parent.parent.parent))
+from nodeps import IPYTHON_EXTENSIONS, MyPrompt  # noqa: E402
+
+sys.path.pop(0)
 
 config = get_config()  # type: ignore[attr-defined]  # noqa: F821
 config.TerminalInteractiveShell.banner1 = ""
