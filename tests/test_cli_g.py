6f4c5d4d837d479f7ff3fc2e3849a5b021f3788f
root <root@example.com>
2023-10-19 22:10:27 +0200

    feat:
    Add support for specific repositories
    
    Added support for Assembla, Bitbucket, FriendCode, Github, Gitlab and BasePlatform repositories. The structure of each added platform consists of configurations related to formatters, patterns, defaults, etc. for their respective functionalities. An init file was also added to assemble these platforms together. This will allow the code to handle links from other git services more efficiently.
    
    Additionally, an XML file to the RuffConfigService and a test file for parsing git URLs was included. They are necessary for internal instance configurations and ensuring stable functionality of repository support across these services.

--- tests/test_cli_g.py
+++ tests/test_cli_g.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import pytest
 
 from nodeps.__main__ import g
@@ -8,4 +6,3 @@ from nodeps.__main__ import g
 @pytest.mark.parametrize("invoke", [[g, "admin"]], indirect=True)
 def test_admin(invoke):
     assert invoke.exit_code == 0
-
