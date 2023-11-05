afa8658ef4030340cc335dd0ba52759d5d5deb36
root <root@example.com>
2023-11-05 16:11:42 +0100

    Refactor package import path and adjust pip installation in workflow
    The path reference in tests/test_docker.py was too dependent on global objects. It was better to directly reference the path from the current file. So, the import path has been changed from 'os' module to 'pathlib' module and the global object 'NODEPS_PATH' has been removed. Similarly, in .github/workflows/main.yml, the code type for pip install command was incorrect, causing dependency installation failure. This was corrected to a single-line command that installs necessary dependencies.

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -1,15 +1,15 @@
-import os
+import pathlib
 import subprocess
 
 import pytest
 
-from nodeps import DOCKER_COMMAND, NODEPS_PATH, PY_MAJOR_MINOR
+from nodeps import DOCKER_COMMAND, PY_MAJOR_MINOR
 
 
 @pytest.mark.skipif(not DOCKER_COMMAND, reason="running already in a container")
 def test_docker():
     assert DOCKER_COMMAND is True
-    path = NODEPS_PATH.parent.parent
+    path = pathlib.Path(__file__).parent.parent
     tag = f"tests-{path.name}"
     assert subprocess.run(
         f"docker build --build-arg='PY_VERSION={PY_MAJOR_MINOR}' --quiet -f {path / 'tests.dockerfile'} -t {tag} "
