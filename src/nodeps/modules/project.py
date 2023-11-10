a2d83355d9456baeab4252c6c08fd10a16a4e03e
root <root@example.com>
2023-11-10 13:30:47 +0100

    Add NODEPS_PATH to project.py test case
    Adjusted the test case in project.py to include the global constant 'NODEPS_PATH', alongside 'NODEPS_TOP', in the repos() function call. This addition aligns the test with the actual application context, aiding in codebase consistency and future testing scenarios.

--- src/nodeps/modules/project.py
+++ src/nodeps/modules/project.py
@@ -482,9 +482,9 @@ class Project:
 
         Examples:
             >>> from nodeps import Project
-            >>> from nodeps import NODEPS_PROJECT_NAME, Path, NODEPS_TOP
+            >>> from nodeps import NODEPS_PROJECT_NAME, Path, NODEPS_TOP, NODEPS_PATH
             >>>
-            >>> Project.repos(), NODEPS_TOP
+            >>> Project.repos(), NODEPS_TOP, NODEPS_PATH
             >>> assert NODEPS_PROJECT_NAME in Project.repos()
             >>> assert NODEPS_PROJECT_NAME in Project.repos(ProjectRepos.DICT)
             >>> assert NODEPS_PROJECT_NAME in Project.repos(ProjectRepos.INSTANCES)
